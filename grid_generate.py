3c005c1be82d0806ce82062cb2efff07644c5792
jj-foster <jonathanfoster46@gmail.com>
2022-11-25 01:16:36 +0000

    bounding box

--- grid_generate.py
+++ grid_generate.py
@@ -2,7 +2,7 @@ import numpy as np
 import json
 import matplotlib.pyplot as plt
 
-def cuboid(data):
+def cuboid_filled(data:dict)->np.ndarray:
     corner = np.array(data["corner"])
     delta = np.array(data["delta"])
     x_count,y_count,z_count = data["count"]
@@ -23,7 +23,7 @@ def cuboid(data):
     else:
         zs=[z0]
 
-    if 0 not in [x_count,y_count,z_count]:
+    if 0 not in [x_count,y_count,z_count] and 1 not in [x_count,y_count,z_count]:
         raise ValueError("2D planes only")
 
     coords=[]
@@ -36,7 +36,96 @@ def cuboid(data):
 
     return coords
 
-def cylinder(data):
+def cuboid_empty(data:dict)->np.ndarray:
+    corner = np.array(data["corner"])
+    delta = np.array(data["delta"])
+    x_count,y_count,z_count = data["count"]
+
+    # determines dimensions of bounding box
+    D=0
+    for i in [x_count,y_count,z_count]:
+        if i>1:
+            D+=1
+
+    """
+         B2-C2      y  z
+      /  A2-D2      | / 
+    B1-C1  /        |___x 
+    A1-D1
+    """
+    A1=corner
+    B1=corner + np.array([0,delta[1],0])
+    C1=corner + np.array([delta[0],delta[1],0])
+    D1=corner + np.array([delta[0],0,0])
+    A2=A1     + np.array([0,0,delta[2]])
+    B2=B1     + np.array([0,0,delta[2]])
+    C2=C1     + np.array([0,0,delta[2]])
+    D2=D1     + np.array([0,0,delta[2]])
+
+    # 2D ie. a rectangular ring.
+    if D==2:
+        coords=[]
+        coords.append(np.linspace(A1,B1,y_count))
+        coords.append(np.linspace(B1,C1,x_count))
+        coords.append(np.linspace(C1,D1,y_count))
+        coords.append(np.linspace(D1,A1,x_count))
+        coords.append(np.linspace(A1,A2,z_count))
+        coords.append(np.linspace(B1,B2,z_count))
+        coords.append(np.linspace(C1,C2,z_count))
+        coords.append(np.linspace(D1,D2,z_count))
+        coords.append(np.linspace(A2,B2,y_count))
+        coords.append(np.linspace(B2,C2,x_count))
+        coords.append(np.linspace(C2,D2,y_count))
+        coords.append(np.linspace(D2,A2,x_count))
+
+        coords_=[]
+        for a in coords:
+            for b in a:
+                if list(b) not in coords_:
+                    coords_.append(list(b))
+                
+        coords=np.array(coords_)
+    
+    # 3D ie. a box.
+    elif D==3:
+
+        def replace(array,x,i):
+            array[:,i]+=x
+            return array
+
+        bottom={"corner":A1,"delta":[delta[0],delta[1],0],"count":[x_count,y_count,1]}    # A1B1C1D1 surface
+        coords_bottom=cuboid_filled(bottom)
+        coords_top=replace(coords_bottom.copy(),delta[2],2)
+
+        front={"corner":A1,"delta":[delta[0],0,delta[2]],"count":[x_count,1,z_count]}    # A1A2D2D1 surface
+        coords_front=cuboid_filled(front)
+        coords_back=replace(coords_front.copy(),delta[1],1)
+
+        left={"corner":A1,"delta":[0,delta[1],delta[2]],"count":[1,y_count,z_count]}    # A1A2B2B1 surface
+        coords_left=cuboid_filled(left)
+        coords_right=replace(coords_left.copy(),delta[0],0)
+
+        coords=np.concatenate((
+            coords_bottom,
+            coords_top,
+            coords_front,
+            coords_back,
+            coords_left,
+            coords_right
+        ))
+
+        coords_=[]
+        for coord in coords:
+            if list(coord) not in coords_:
+                coords_.append(list(coord))
+        coords=np.array(coords_)
+
+    else:
+        raise Exception("Grid definition error: 1D not supported for empty cuboids.")
+
+    return coords
+
+def cylinder(data:dict)->np.ndarray:
 
     origin      =   np.array(data["origin"])
     axis        =   np.array(data["axis"])
@@ -105,6 +194,7 @@ def plot_grid(coords):
         ax.set_xlabel("x")
         ax.set_ylabel("y")
         ax.set_zlabel("z")
+        plt.tight_layout()
 
     plt.show()
 
@@ -113,6 +203,12 @@ def export_grid(run,wake,coords,file_type):
     if file_type==".scn":
         lines=[f"{run} {wake}\n"]
     elif file_type==".qo" or file_type==".qom" or file_type==".qpt":
+        coords_=[]
+        for coord in coords:
+            if list(coord) not in coords_:
+                coords_.append(list(coord))
+        coords=np.array(coords_)
+
         lines=[f"{int(len(coords))}\n"]
     else:
         raise Exception(f"{file_type} is not a valid file type.")
@@ -125,20 +221,15 @@ def export_grid(run,wake,coords,file_type):
 
     return None
 
-def const_qo(coords):
+def const_qo(coords,dV):
 
-    du  = np.full(len(coords),0)
-    dv  = np.full(len(coords),0)
-    dw  = np.full(len(coords),0)
-    dcp = np.full(len(coords),-10)
+    du  = np.full(len(coords),dV[0])
+    dv  = np.full(len(coords),dV[1])
+    dw  = np.full(len(coords),dV[2])
+    dcp = np.full(len(coords),dV[3])
     
     coords=coords.T
 
-    # coords = np.append(coords,du,axis=0)
-    # coords = np.append(coords,dv,axis=0)
-    # coords = np.append(coords,dw,axis=0)
-    # coords = np.append(coords,dcp,axis=0)
-    
     coords=coords.tolist()
     coords.append(du)
     coords.append(dv)
@@ -149,39 +240,49 @@ def const_qo(coords):
 
     return coords
 
+def del_NEWPAN_runs(directory:str):
+    pass
+
 if __name__=="__main__":
 
-    proj_name="wing"
-    directory="D:\\Documents\\University\\NEWPAN VM\\VMDrive2_120122\\VMDrive2\\DataVM2\\Projects\\6_qo\\1_qo\\"
-    grid_def="grids/wing_qo_xz.json"
+    proj_name="EDF"
+    directory="D:\\Documents\\University\\NEWPAN VM\\VMDrive2_120122\\VMDrive2\\DataVM2\\Projects\\3_EDF\\4_EDF_qo\\"
+
+    grid_def=["grids/EDF_bb.json"]
 
     run=1
-    wake=0
+    wake=1
 
     plot=True
-    export=True
+    export=False
 
-    file_type=".qpt"
+    file_type=".qo"
 
     ####################################################################################
 
-    with open(grid_def,'r') as f:
-        data=json.load(f)
+    coords=[]
+    for grid in grid_def:
 
-    if data["type"]=="cylinder":
-        coords=cylinder(data)
-    elif data["type"]=="cuboid":
-        coords=cuboid(data)
-    else:
-        raise ValueError("Invalid grid type.")
+        with open(grid,'r') as f:
+            data=json.load(f)
 
+        if data["type"]=="cylinder":
+            coords.append(cylinder(data))
+        elif data["type"]=="cuboid":
+            if data["filled"]==True:
+                coords.append(cuboid_filled(data))
+            else:
+                coords.append(cuboid_empty(data))
+        else:
+            raise ValueError("Invalid grid type.")
 
-    if export==True:
-        if file_type==".qo" or file_type==".qom":
-            coords=const_qo(coords)
-            
-        export_grid(run,wake,coords,file_type)
+    coords=np.concatenate([coords[i] for i in range(len(coords))])
 
     if plot==True:
         plot_grid(coords)
     
+    if export==True:
+        if file_type==".qo" or file_type==".qom":
+            coords=const_qo(coords,data["qo"])
+            
+        export_grid(run,wake,coords,file_type)
